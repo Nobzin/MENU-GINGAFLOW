a4a76d656b3960923066841bd2849ab4c25869a5
Julio Cesar <cesargameplays10@gmail.com>
2026-06-07 00:19:12 -0300

    atualização_menu

--- MENU/SCENES/audio_manager.gd
+++ MENU/SCENES/audio_manager.gd
@@ -1,4 +1,6 @@
 extends AudioStreamPlayer2D
+var musicas = []
+var nome = " L’Étoile D’Afrique x Les Trompettes D’Afrique"
 
 func stop_music():
 	stop()
--- MENU/SCRIPTS/menu.gd
+++ MENU/SCRIPTS/menu.gd
@@ -1,5 +1,20 @@
 extends Control
-
+@onready var music_text = $Now_playing/music_label
+@onready var musica_atual = get_node("/root/AudioManager")
+var loading_screen = preload("res://Carregamento.tscn")
+func _ready() -> void:
+	var loading = loading_screen.instantiate()
+	get_tree().root.add_child(loading)
+	music_text.text+= musica_atual.nome
+	musica_atual.start_music()
+var hue:= 0.0
+func _process(delta: float) -> void:
+	hue += delta * 0.3
+	
+	if hue > 1.0:
+		hue = 0.0
+	music_text.modulate = Color.from_hsv(hue,1.0,1.0)
+	
 
 func _on_button_pressed() -> void:
 	pass # Replace with function body.
@@ -26,3 +41,4 @@ func _on_button_quit_pressed() -> void:
 
 func _on_button_login_pressed() -> void:
 	get_tree().change_scene_to_file("res://MENU/SCENES/login.tscn")
+	
